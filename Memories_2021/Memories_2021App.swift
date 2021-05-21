4bed52db2ce7ced30bfd539a789404c52809556e
HY <pride508@gmail.com>
2021-05-21 22:15:01 +0800

    Initial Commit

---
+++ Memories_2021/Memories_2021App.swift
@@ -0,0 +1,17 @@
+//
+//  Memories_2021App.swift
+//  Memories_2021
+//
+//  Created by HY Chuang on 2021/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Memories_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
